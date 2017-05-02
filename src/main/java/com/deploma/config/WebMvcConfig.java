f3fc53aa9e25f3021047cbfa3fb5e2bf6bfb831e
vadim <zavertal.v@gmail.com>
2017-05-03 02:33:53 +0300

    check  static

--- src/main/java/com/deploma/config/WebMvcConfig.java
+++ src/main/java/com/deploma/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.deploma.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -30,6 +29,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("**/resources/**").addResourceLocations("**/resources/");
     }
 }
--- src/main/webapp/jsp/home.jsp
+++ src/main/webapp/jsp/home.jsp
@@ -1,7 +1,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
-    <link rel="stylesheet" href="../resources/css/style.css">
+    <link rel="stylesheet" href="<c:url value="/static/css/style.css"/>">
     <title>Home</title>
 </head>
 <body>
