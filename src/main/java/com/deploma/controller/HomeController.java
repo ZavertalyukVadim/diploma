06881c6649e14dade98fdd6a864946d141ef5ec6
vadim <zavertal.v@gmail.com>
2017-05-09 01:16:11 +0300

    add absolute pass

--- src/main/java/com/deploma/controller/HomeController.java
+++ src/main/java/com/deploma/controller/HomeController.java
@@ -1,27 +1,14 @@
 package com.deploma.controller;
 
-import com.deploma.entity.TestEntity;
-import com.deploma.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
-    private final TestService testService;
-
-    @Autowired
-    public HomeController(TestService testService) {
-        this.testService = testService;
-    }
 
     @GetMapping(value = "/")
     public String homePage(ModelMap modelMap){
-        modelMap.addAttribute("tests", testService.getTestData());
         return "home";
     }
 }
--- src/main/java/com/deploma/dao/TestDao.java
+++
@@ -1,7 +0,0 @@
-package com.deploma.dao;
-
-import com.deploma.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestDao extends JpaRepository<TestEntity,Integer>{
-}
